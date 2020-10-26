31b5c988ee16716ca2f5bdd7c2df9a3d7f33e783
Daphne-wu <71472654+Daphne-wu@users.noreply.github.com>
2020-10-25 19:17:26 -0700

    Create digitalMedia.cpp

---
+++ digitalMedia.cpp
@@ -0,0 +1,13 @@
+//digital media cpp file with all methods PARENT CLASS
+#include "digitalMedia.h"
+
+using namespace std;
+
+digitalMedia::digitalMedia(string tit, int yea) {
+	title = tit;
+	year = yea;
+}
+
+digitalMedia::digitalMedia() {
+
+}
